3ecce29d9d803f8adbf011b59062e007096d0053
dnguyen220022@gmail.com <dnguyen220022@gmail.com>
2025-08-30 12:54:03 +1000

    refix file structre; install tailwind 4 + daisy

---
+++ climatise-app/src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+  <div className="min-h-screen flex justify-center items-center">
+    <div className="card w-96 bg-black shadow-xl">
+      <div className="card-body">
+        <h2 className="card-title">Climatise Test Card</h2>
+        <p>This page is for Climatise</p>
+        <div className="card-actions justify-end">
+          <button className="btn btn-primary">This button does nothing</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  )
+}
+
+export default App
