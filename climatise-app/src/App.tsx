1c4af9b1c7c3b89e3f3293cac08360743135404a
Daniel Nguyen <dnguyen220022@gmail.com>
2025-09-04 20:00:02 +1000

    update gitignore + fix light/dark theming

--- climatise-app/src/App.tsx
+++ climatise-app/src/App.tsx
@@ -1,48 +1,43 @@
-import { useState } from 'react'
-import { NavBar } from './components/NavBar'
+import { useState } from "react";
+import { NavBar } from "./components/NavBar";
 
 function App() {
   return (
     <div className="flex flex-col w-screen h-screen">
-
       {/* NavBar */}
       <div className="NavBar w-full">
-        <NavBar/>
+        <NavBar />
       </div>
 
       {/* Main Content */}
-      
-      <div className= "mainContentWrapper flex w-full h-full flex-1">
 
-        <div className = "vizColumn flex flex-col w-2/3 h-full">
-          <div className = "mainViz flex w-full h-4/6 p-2">
-            <span className = "border-2 h-full w-full">Main Viz</span>
+      <div className="mainContentWrapper flex w-full h-full flex-1">
+        <div className="vizColumn flex flex-col w-2/3 h-full">
+          <div className="mainViz flex w-full h-4/6 p-2">
+            <span className="border-2 h-full w-full">Main Viz</span>
           </div>
 
-          <div className = "secondaryViz flex w-full h-2/6 p-2">
-            <span className = "border-2 h-full w-full">Secondary Viz</span>
-          </div>
-        </div>
-
-        <div className = "filterColumn flex flex-col w-2/6 h-full">
-          <div className = "dateFilter flex w-full h-2/6 p-2">
-            <span className = "border-2 h-full w-full">Date Filter/Calender</span>
-          </div>
-
-          <div className = "stationFilter flex w-full h-2/6 p-2">
-            <span className = "border-2 h-full w-full">Weather Station Filter</span>
-          </div>
-
-          <div className = "fillerSpace flex w-full h-2/6 p-2">
-            <span className = "border-2 h-full w-full">Filler Space</span>
+          <div className="secondaryViz flex w-full h-2/6 p-2">
+            <span className="border-2 h-full w-full">Secondary Viz</span>
           </div>
         </div>
 
+        <div className="filterColumn flex flex-col w-2/6 h-full">
+          <div className="dateFilter flex w-full h-2/6 p-2">
+            <span className="border-2 h-full w-full">Date Filter/Calender</span>
+          </div>
+
+          <div className="stationFilter flex w-full h-2/6 p-2">
+            <span className="border-2 h-full w-full">Weather Station Filter</span>
+          </div>
+
+          <div className="fillerSpace flex w-full h-2/6 p-2">
+            <span className="border-2 h-full w-full">Filler Space</span>
+          </div>
+        </div>
       </div>
-
-
-    </div>    
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
--- climatise-app/src/components/NavBar.tsx
+++ climatise-app/src/components/NavBar.tsx
@@ -1,23 +1,29 @@
 export function NavBar() {
-    return (
-        <div className="navbar bg-base-100 shadow-sm">
-        <div className="flex-1">
-            <a className="btn btn-ghost text-xl">Climatise</a>
-        </div>
-        <div className="flex-none">
-            <ul className="menu menu-horizontal px-1">
-            <li><a>Our Mission</a></li>
-            <li>
-                <details>
-                <summary>Something Else goes here</summary>
-                <ul className="bg-base-100 rounded-t-none p-2">
-                    <li><a>Link 1</a></li>
-                    <li><a>Link 2</a></li>
-                </ul>
-                </details>
-            </li>
-            </ul>
-        </div>
-        </div>
-    )
+  return (
+    <div className="navbar bg-base-100 shadow-sm">
+      <div className="flex-1">
+        <a className="btn btn-ghost text-xl">Climatise</a>
+      </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <a>Our Mission</a>
+          </li>
+          <li>
+            <details>
+              <summary>Something Else Goes Here</summary>
+              <ul className="bg-base-100 rounded-t-none p-2">
+                <li>
+                  <a>Link 1</a>
+                </li>
+                <li>
+                  <a>Link 2</a>
+                </li>
+              </ul>
+            </details>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
