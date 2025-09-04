441c66b7d6878ffd03f29b267359a4d20a4eaa58
Daniel Nguyen <dnguyen220022@gmail.com>
2025-09-04 15:35:47 +1000

    change grid to flexbox, updated basic template with borders and labels, now they just need to be filled in with our viz/filters

--- climatise-app/src/App.tsx
+++ climatise-app/src/App.tsx
@@ -6,26 +6,41 @@ function App() {
     <div className="flex flex-col w-screen h-screen">
 
       {/* NavBar */}
-      <div className="NavBar">
+      <div className="NavBar w-full">
         <NavBar/>
       </div>
 
       {/* Main Content */}
-      <div className="flex-1 p-4">
-        {/* grid setup */}
-        <div className="grid grid-cols-3 grid-rows-3 gap-1 h-full">
-          <p>1</p>
-          <p>2</p>
-          <p>3</p>
-          <p>4</p>
-          <p>5</p>
-          <p>6</p>
-          <p>7</p>
-          <p>8</p>
-          <p>9</p>
+      
+      <div className= "mainContentWrapper flex w-full h-full flex-1">
+
+        <div className = "vizColumn flex flex-col w-2/3 h-full">
+          <div className = "mainViz flex w-full h-4/6 p-2">
+            <span className = "border-2 h-full w-full">Main Viz</span>
+          </div>
+
+          <div className = "secondaryViz flex w-full h-2/6 p-2">
+            <span className = "border-2 h-full w-full">Secondary Viz</span>
+          </div>
+        </div>
+
+        <div className = "filterColumn flex flex-col w-2/6 h-full">
+          <div className = "dateFilter flex w-full h-2/6 p-2">
+            <span className = "border-2 h-full w-full">Date Filter/Calender</span>
+          </div>
+
+          <div className = "stationFilter flex w-full h-2/6 p-2">
+            <span className = "border-2 h-full w-full">Weather Station Filter</span>
+          </div>
+
+          <div className = "fillerSpace flex w-full h-2/6 p-2">
+            <span className = "border-2 h-full w-full">Filler Space</span>
+          </div>
         </div>
 
       </div>
+
+
     </div>    
   )
 }
